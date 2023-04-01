7863e6ca9de455993baa4610b3c586c77fdf1a8b
patrykjakimczyk <patr.jakimczyk@gmail.com>
2023-04-01 16:56:48 +0200

    Deleted useless webconfig class, corrected pesel validation

--- src/main/java/pl/medical/visits/util/ValidationUtil.java
+++ src/main/java/pl/medical/visits/util/ValidationUtil.java
@@ -2,6 +2,8 @@ package pl.medical.visits.util;
 
 import pl.medical.visits.model.user.User;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Locale;
 
 public class ValidationUtil {
@@ -18,9 +20,10 @@ public class ValidationUtil {
 
         char[] pArray = pesel.toCharArray();
 
+        if (!isBirthDayEqualPesel(user)) return false;
         if (!isPeselSumValid(pArray)) return false;
-        if(pArray[10] % 2 == 0 && sex.equals("female")) return true;
-        return pArray[10] % 2 != 0 && sex.equals("male");
+        if(pArray[10] % 2 == 0 && sex.equals("Female")) return true;
+        return pArray[10] % 2 != 0 && sex.equals("Male");
     }
 
     private static boolean isPeselSumValid(char[] pArray) {
@@ -40,5 +43,35 @@ public class ValidationUtil {
         sum %= 10;
 
         return sum == Character.getNumericValue(pArray[10]);
+    }
+
+    private static boolean isBirthDayEqualPesel(User user) {
+        String birthDate[] = user.getBirthDate().split("-");
+        String pesel = user.getPesel();
+        String peselYear = pesel.substring(0, 2);
+        int peselMonth = Integer.parseInt(pesel.substring(2, 4));
+        String peselDay = pesel.substring(4, 6);
+        LocalDate currentDate = LocalDate.now();
+        LocalDate birthDay = LocalDate.of(
+                Integer.parseInt(birthDate[0]),
+                Integer.parseInt(birthDate[1]),
+                Integer.parseInt(birthDate[2])
+        );
+
+        if (Period.between(birthDay, currentDate).getYears() < 18) return false;
+
+        if (peselMonth >= 21 && peselMonth < 33) {
+            peselMonth -= 20;
+        } else if (peselMonth >= 41 && peselMonth < 53) {
+            peselMonth -= 40;
+        } else if (peselMonth >= 61 && peselMonth < 73) {
+            peselMonth -= 60;
+        } else if (peselMonth >= 81 && peselMonth < 93) {
+            peselMonth -= 80;
+        } else if(peselMonth > 12 && peselMonth < 21) return false;
+
+        if (!birthDate[2].equals(peselDay)) return false;
+        if (Integer.parseInt(birthDate[1]) != peselMonth) return false;
+        return birthDate[0].substring(2).equals(peselYear);
     }
 }
